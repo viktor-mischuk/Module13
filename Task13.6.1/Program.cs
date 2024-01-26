20b8467c5f798cea69ee75bac849ae55a8f77e4c
Handy <mvv0864@gmail.com>
2024-01-26 20:42:19 +0300

    Добавьте файлы проекта.

---
+++ Task13.6.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task13._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sr = new StreamReader("Task13.6.1.txt"))
+            {
+                List<string> strList = new List<string>();
+                LinkedList<string> strLinkedList = new LinkedList<string>();
+
+                // Читаем файл
+                var text = sr.ReadToEnd();
+
+                //разбиваем строку по пробелам
+                var wordsT = text.Split(" ");
+                ////Убираем знаки пунктуации
+                var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+                // запускаем новый таймер
+                var stopWatch = Stopwatch.StartNew();
+                strList.AddRange(wordsT);
+                stopWatch.Stop();
+
+                // запускаем новый таймер
+                var stopWatchLinked = Stopwatch.StartNew();
+                foreach (var word in wordsT)
+                {
+                    strLinkedList.AddLast(word);
+                }
+                
+                stopWatchLinked.Stop();
+
+
+                // смотрим, сколько операция заняла, в миллисекундах
+                Console.WriteLine($"Не связный список : {stopWatch.Elapsed.TotalMilliseconds} мс");
+                Console.WriteLine();
+
+                Console.WriteLine($"Связный список : {stopWatchLinked.Elapsed.TotalMilliseconds} мс");
+                Console.ReadKey();
+            }           
+        }
+    }
+}
---
+++ Task13.6.2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+
+namespace Task13._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wordsList = new List<string>();
+            Dictionary<string, int> wordDict = new Dictionary<string, int>();
+
+            using (var sr = new StreamReader("Task13.6.2.txt"))
+            {
+                string line = null;
+                while(null != (line = sr.ReadLine()))
+                {
+                    //разбиваем строку по пробелам
+                    var wordsT = line.Split(" ");
+
+                    string wordsTT;
+                    foreach(var word in wordsT)
+                    {
+                        //только юуквы
+                        wordsTT = new string(word.Where(c => char.IsLetter(c)).ToArray());
+                        if (string.IsNullOrEmpty(wordsTT)) continue;
+                        // Если один символ, считаем что это не слово (союзы не считаем)
+                        if(wordsTT.Length < 2) continue;
+                        //все к верхнему регистру для единообразия
+                        wordsList.Add(wordsTT.ToUpper());
+                    }
+
+                }
+                //Упорядчиваем список
+                wordsList.Sort();
+
+                //счетчик вхождений
+                int i = 0;
+                //текущее слово
+                var curr = wordsList[0];
+                foreach (var word in wordsList)
+                {
+                    if (word == curr) i++; 
+                    else
+                    {
+                        //словарь  -ключ слово - значение частота вхождения
+                        wordDict.Add(curr, i);
+                        curr = word;
+                        i = 1;
+                    }
+                }
+            }
+            // список частот вхождения
+            List<int> enterNumbers = new List<int>();
+            foreach (var value in wordDict.Values)
+            {
+                enterNumbers.Add(value);
+            }
+            // сортируум по значению
+            enterNumbers.Sort();
+            // удаляем дубликаты
+            enterNumbers.Distinct();
+            // разворачиваем от большего к меньшему
+            enterNumbers.Reverse();
+            // 10 наибольших значений частоты вхождения
+            var enterRange = enterNumbers.GetRange(0, 10);
+            
+            foreach(var item in enterRange)
+            {
+                int counter = 0;
+                string str = null;
+                foreach (var key in wordDict.Keys) 
+                { 
+                    if(wordDict[key] != item) { continue; }
+                    counter = wordDict[key];
+                    str = key;
+                }
+                Console.WriteLine($"Слово '{str}'     используется {counter} раз");
+            }
+            Console.ReadKey();
+        }
+    }
+}
